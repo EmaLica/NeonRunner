45a21b40a7416169081739e1f8ed9f1c293587e0
Emanuele Licata <emanuelelicatamail@gmail.com>
2023-11-11 00:25:53 +0100

    friday commit :-1:

--- src/main/java/main/GameWindow.java
+++ src/main/java/main/GameWindow.java
@@ -9,17 +9,16 @@ public class GameWindow {
     private JFrame jframe;
 
     public GameWindow(GamePanel gamePanel) {
-
         jframe = new JFrame();
 
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.add(gamePanel);
-        jframe.setLocationRelativeTo(null);
         jframe.setResizable(false);
         jframe.pack();
+        jframe.setLocationRelativeTo(null);
         jframe.setVisible(true);
-        jframe.addWindowFocusListener(new WindowFocusListener() {
 
+        jframe.addWindowFocusListener(new WindowFocusListener() {
             @Override
             public void windowLostFocus(WindowEvent e) {
                 gamePanel.getGame().windowFocusLost();
@@ -28,10 +27,7 @@ public class GameWindow {
             @Override
             public void windowGainedFocus(WindowEvent e) {
                 // TODO Auto-generated method stub
-
             }
         });
-
     }
-
 }
